cbff28ce69e4c1e3a2b4ecb342b9175f20c7287c
shota <matsuo0416877@gmail.com>
2021-05-23 17:01:18 +0900

    initial commit

---
+++ ScheduleLight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScheduleLight
+//
+//  Created by 松尾祥汰 on 2021/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScheduleLight/ScheduleLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleLightApp.swift
+//  ScheduleLight
+//
+//  Created by 松尾祥汰 on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
